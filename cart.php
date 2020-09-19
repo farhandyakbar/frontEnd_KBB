953fc416e7f7d6dac4acb4aef0caedafe2d98b39
farhan <farhandy99@gmail.com>
2020-09-19 20:29:41 +0700

    tolong mas ngge

--- cart.php
+++ cart.php
@@ -41,35 +41,130 @@
                             <form action="#">
                                 <div class="table-content table-responsive">
                                     <table>
-                                        <thead>
-                                            <tr>
-                                                <th class="product-thumbnail">Image</th>
-                                                <th class="product-name">Product</th>
-                                                <th class="product-price">Price</th>
-                                                <th class="product-quantity">Quantity</th>
-                                                <th class="product-subtotal">Total</th>
-                                                <th class="product-remove">Remove</th>
-                                            </tr>
-                                        </thead>
                                         <tbody>
-                                            <tr>
-                                                <td class="product-thumbnail"><a href="#"><img
-                                                            src="images/product/4.png" alt="product img" /></a></td>
-                                                <td class="product-name"><a href="#">Vestibulum suscipit</a></td>
-                                                <td class="product-price"><span class="amount">£165.00</span></td>
-                                                <td class="product-quantity"><input type="number" value="1" /></td>
-                                                <td class="product-subtotal">£165.00</td>
-                                                <td class="product-remove"><a href="#">X</a></td>
-                                            </tr>
+                                        <?php 
+                if(!empty($_GET["action"])) {
+                switch($_GET["action"]) {
+                    case "add":
+                        if(!empty($_POST["quantity"])) {
+                        // IMAGE	PRODUCT	PRICE	QUANTITY	TOTAL	
+                            $id =  $_GET["code"]; 
+                            $sqlSelect = "SELECT barang.*, kategori.nama as nama_kategori FROM barang join kategori on barang.id_kategori=kategori.id where barang.id=$id";
+                            $resultSelect = mysqli_query($conn, $sqlSelect);
+                            $detailBarang = mysqli_fetch_assoc($resultSelect);  
+
+                            $sqlSelect = "SELECT gambar FROM barang_detail  where id_barang=".$detailBarang['id'] ;
+                            $resultSelect = mysqli_query($conn, $sqlSelect);
+                            $gambar = mysqli_fetch_assoc($resultSelect);         
+                            // $productByCode = $db_handle->runQuery("SELECT * FROM tblproduct WHERE code='" . $_GET["code"] . "'");
+                            $itemArray = array($detailBarang['id']=>array('name'=>$detailBarang["nama"], 'kode'=>$id, 'quantity'=>$_POST["quantity"], 'price'=>$detailBarang['harga'], 'image'=> $gambar['gambar']));
+                            
+                            if(!empty($_SESSION["cart_item"])) {
+                                if(in_array($detailBarang['id'],array_keys($_SESSION["cart_item"]))) {
+                                    foreach($_SESSION["cart_item"] as $k => $v) {
+                                            if($detailBarang['id'] == $k) {
+                                                if(empty($_SESSION["cart_item"][$k]["quantity"])) {
+                                                    $_SESSION["cart_item"][$k]["quantity"] = 0;
+                                                }
+                                                $_SESSION["cart_item"][$k]["quantity"] += $_POST["quantity"];
+                                            }
+                                    }
+                                } else {
+                                    $_SESSION["cart_item"] = array_merge($_SESSION["cart_item"],$itemArray);
+                                }
+                            } else {
+                                $_SESSION["cart_item"] = $itemArray;
+                            }
+                        }
+                    break;
+                    case "remove":
+                        if(!empty($_SESSION["cart_item"])) {
+                            foreach($_SESSION["cart_item"] as $k => $v) {
+                                    if($_GET["code"] == $k)
+                                        unset($_SESSION["cart_item"][$k]);				
+                                    if(empty($_SESSION["cart_item"]))
+                                        unset($_SESSION["cart_item"]);
+                            }
+                        }
+                    break;
+                    case "empty":
+                        unset($_SESSION["cart_item"]);
+                    break;	
+                }
+                }
+                ?>
+            <HTML>
+            <!-- <HEAD> -->
+            <TITLE>Simple PHP Shopping Cart</TITLE>
+            <link href="style.css" type="text/css" rel="stylesheet" />
+            </HEAD>
+            <BODY>
+                <div id="shopping-cart">
+                    <div class="txt-heading">Shopping Cart</div>
+                    <?php
+                                if(isset($_SESSION["cart_item"])){
+                                $total_quantity = 0;
+                                $total_price = 0;
+                            ?>
+                    <table class="tbl-cart" cellpadding="10" cellspacing="1">
+                        <tbody>
+                            <tr>
+                                <th style="text-align:left;">Name</th>
+                                <th style="text-align:left;">Code</th>
+                                <th style="text-align:right;" width="5%">Quantity</th>
+                                <th style="text-align:right;" width="10%">Unit Price</th>
+                                <th style="text-align:right;" width="10%">Price</th>
+                                <th style="text-align:center;" width="5%">Remove</th>
+                            </tr>
+                            <?php		
+                                foreach ($_SESSION["cart_item"] as $item){
+                                $item_price = $item["quantity"]*$item["price"];
+                            ?>
+                            <tr>
+                                <td><img src="<?php echo $item["image"]; ?>"
+                                        class="cart-item-image" /><?php echo $item["name"]; ?></td>
+                                <td><?php echo $item["kode"]; ?></td>
+                                <td style="text-align:right;"><?php echo $item["quantity"]; ?></td>
+                                <td style="text-align:right;"><?php echo "Rp ".$item["price"]; ?></td>
+                                <td style="text-align:right;"><?php echo "Rp ". number_format($item_price,2); ?></td>
+                                <td style="text-align:center;"><a
+                                        href="index.php?action=remove&code=<?php echo $item["code"]; ?>"
+                                        class="btnRemoveAction"><img src="icon-delete.png" alt="Remove Item" /></a></td>
+                            </tr>
+                            <?php
+                                $total_quantity += $item["quantity"];
+                                $total_price += ($item["price"]*$item["quantity"]);
+                            }
+                            ?>
+                            <tr>
+                                <td colspan="2" align="right">Total:</td>
+                                <td align="right"><?php echo $total_quantity; ?></td>
+                                <td align="right" colspan="2">
+                                    <strong><?php echo "Rp ".number_format($total_price, 2); ?></strong></td>
+                                <td></td>
+                            </tr>
+                        </tbody>
+                    </table>
+                    <?php
+                } else {
+                ?>
+                    <div class="no-records">Your Cart is Empty</div>
+                    <?php 
+                }
+                ?>
+                </div>
                                         </tbody>
                                     </table>
                                 </div>
                                 <div class="row">
                                     <div class="col-md-8 col-sm-7 col-xs-12">
-                                        <div class="buttons-cart">
+                                        <!-- <div class="buttons-cart">
                                             <input type="submit" value="Update Cart" />
                                             <a href="#">Continue Shopping</a>
-                                        </div>
+                                        </div> -->
+                                    <div class="buttons-cart">
+                                        <a id="btnEmpty" href="cart.php?action=empty">Empty Cart</a>
+                                    </div>
                                     </div>
                                     <div class="col-md-4 col-sm-5 col-xs-12">
                                         <div class="cart_totals">
@@ -123,118 +218,7 @@
             </div>
             <!-- cart-main-area end -->
 
-            <?php 
-                if(!empty($_GET["action"])) {
-                switch($_GET["action"]) {
-                    case "add":
-                        if(!empty($_POST["quantity"])) {
-                        // IMAGE	PRODUCT	PRICE	QUANTITY	TOTAL	
-                            $id =  $_GET["code"]; 
-                            $sqlSelect = "SELECT barang.*, kategori.nama as nama_kategori FROM barang join kategori on barang.id_kategori=kategori.id where barang.id=$id";
-                            $resultSelect = mysqli_query($conn, $sqlSelect);
-                            $detailBarang = mysqli_fetch_assoc($resultSelect);  
-
-                            $sqlSelect = "SELECT gambar FROM barang_detail  where id_barang=".$detailBarang['id'] ;
-                            $resultSelect = mysqli_query($conn, $sqlSelect);
-                            $gambar = mysqli_fetch_assoc($resultSelect);         
-                            // $productByCode = $db_handle->runQuery("SELECT * FROM tblproduct WHERE code='" . $_GET["code"] . "'");
-                            $itemArray = array($detailBarang['id']=>array('name'=>$detailBarang["nama"], 'kode'=>$id, 'quantity'=>$_POST["quantity"], 'price'=>$detailBarang['harga'], 'image'=> $gambar['gambar']));
-                            
-                            if(!empty($_SESSION["cart_item"])) {
-                                if(in_array($detailBarang['id'],array_keys($_SESSION["cart_item"]))) {
-                                    foreach($_SESSION["cart_item"] as $k => $v) {
-                                            if($detailBarang['id'] == $k) {
-                                                if(empty($_SESSION["cart_item"][$k]["quantity"])) {
-                                                    $_SESSION["cart_item"][$k]["quantity"] = 0;
-                                                }
-                                                $_SESSION["cart_item"][$k]["quantity"] += $_POST["quantity"];
-                                            }
-                                    }
-                                } else {
-                                    $_SESSION["cart_item"] = array_merge($_SESSION["cart_item"],$itemArray);
-                                }
-                            } else {
-                                $_SESSION["cart_item"] = $itemArray;
-                            }
-                        }
-                    break;
-                    case "remove":
-                        if(!empty($_SESSION["cart_item"])) {
-                            foreach($_SESSION["cart_item"] as $k => $v) {
-                                    if($_GET["code"] == $k)
-                                        unset($_SESSION["cart_item"][$k]);				
-                                    if(empty($_SESSION["cart_item"]))
-                                        unset($_SESSION["cart_item"]);
-                            }
-                        }
-                    break;
-                    case "empty":
-                        unset($_SESSION["cart_item"]);
-                    break;	
-                }
-                }
-                ?>
-                    <HTML>
-                    <!-- <HEAD> -->
-                        <TITLE>Simple PHP Shopping Cart</TITLE>
-                        <link href="style.css" type="text/css" rel="stylesheet" />
-                    </HEAD>
-                    <BODY>
-                        <div id="shopping-cart">
-                            <div class="txt-heading">Shopping Cart</div>
-                            <a id="btnEmpty" href="cart.php?action=empty">Empty Cart</a>
-                            <?php
-                if(isset($_SESSION["cart_item"])){
-                    $total_quantity = 0;
-                    $total_price = 0;
-                ?>
-                    <table class="tbl-cart" cellpadding="10" cellspacing="1">
-                        <tbody>
-                            <tr>
-                                <th style="text-align:left;">Name</th>
-                                <th style="text-align:left;">Code</th>
-                                <th style="text-align:right;" width="5%">Quantity</th>
-                                <th style="text-align:right;" width="10%">Unit Price</th>
-                                <th style="text-align:right;" width="10%">Price</th>
-                                <th style="text-align:center;" width="5%">Remove</th>
-                            </tr>
-                            <?php		
-                                foreach ($_SESSION["cart_item"] as $item){
-                                $item_price = $item["quantity"]*$item["price"];
-                            ?>
-                            <tr>
-                                <td><img src="<?php echo $item["image"]; ?>"
-                                        class="cart-item-image" /><?php echo $item["name"]; ?></td>
-                                <td><?php echo $item["kode"]; ?></td>
-                                <td style="text-align:right;"><?php echo $item["quantity"]; ?></td>
-                                <td style="text-align:right;"><?php echo "$ ".$item["price"]; ?></td>
-                                <td style="text-align:right;"><?php echo "$ ". number_format($item_price,2); ?></td>
-                                <td style="text-align:center;"><a
-                                        href="index.php?action=remove&code=<?php echo $item["code"]; ?>"
-                                        class="btnRemoveAction"><img src="icon-delete.png" alt="Remove Item" /></a></td>
-                            </tr>
-                            <?php
-                                $total_quantity += $item["quantity"];
-                                $total_price += ($item["price"]*$item["quantity"]);
-                            }
-                            ?>
-                            <tr>
-                                <td colspan="2" align="right">Total:</td>
-                                <td align="right"><?php echo $total_quantity; ?></td>
-                                <td align="right" colspan="2">
-                                    <strong><?php echo "$ ".number_format($total_price, 2); ?></strong></td>
-                                <td></td>
-                            </tr>
-                        </tbody>
-                    </table>
-                    <?php
-                } else {
-                ?>
-                    <div class="no-records">Your Cart is Empty</div>
-                    <?php 
-                }
-                ?>
-                    </div>
+            
 
 
 
